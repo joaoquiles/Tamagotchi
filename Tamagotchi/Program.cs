957504fa681e95314a3e8861f4e40ae8ac2058b3
joaoquiles <joaov_quiles@hotmail.com>
2023-04-04 14:42:54 -0300

    Adicionar arquivos de projeto.

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+        RestRequest request = new RestRequest("",Method.Get);
+        var response = client.Execute(request);
+
+        Console.WriteLine(response.Content);
+        Console.ReadLine();
+
+
+    }
+
+}
