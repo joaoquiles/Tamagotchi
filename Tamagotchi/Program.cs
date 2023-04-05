792894c4f54200db66cfcfcd56ddd4d16a83f543
joaoquiles <joaov_quiles@hotmail.com>
2023-04-05 20:31:06 -0300

    Leitura de todas as paginas da API e armazenamento dos dados.

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,17 +1,54 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using static System.Net.WebRequestMethods;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        List<ListaDePokemons> todos = new List<ListaDePokemons>();
 
-        var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
-        RestRequest request = new RestRequest("",Method.Get);
-        var response = client.Execute(request);
+        var baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+        while (baseUrl != null)
+        {
+            RestClient client = new RestClient(baseUrl);
+            var request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
 
-        Console.WriteLine(response.Content);
-        Console.ReadLine();
+            ListaDePokemons resposta = JsonConvert.DeserializeObject<ListaDePokemons>(response.Content);
+            todos.Add(resposta);
+            baseUrl = resposta.Next;
+        }
 
+        foreach (var item in todos)
+        {
+            foreach (var pokemon in item.Results)
+            {
+                Console.WriteLine(pokemon.Name);
+            }
+        }
+
+
+    }
+
+    public class ListaDePokemons
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public List<Pokemon> Results { get; set; }
+
+    }
+
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
 
     }
 
